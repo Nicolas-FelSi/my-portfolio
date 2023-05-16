d59bd3e8ede9a99b52eb596ce566de5e54b5abdc
Nicolas Felipe da Silva <nicolasfelipe3702@gmail.com>
2023-05-16 18:34:03 -0300

    adicionado quase todo o conteúdo, e parte dinâmica do portfólio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
-const toggleHeader = document.querySelector(".fa-bars")
+const tagHtml = document.querySelector("html")
+const toggleHeader = document.querySelector('i[class="fa-solid fa-bars"]')
 const navList = document.querySelector('.nav__list')
+const inputDarkMode = document.querySelector('.switch input')
+const imgDarkMode = document.querySelector(".dark")
+const imgLightMode = document.querySelector('.light')
+const imgDarkModeMain = document.querySelector(".img-dark-mode")
+const imglightModeMain = document.querySelector(".img-light-mode")
 
 toggleHeader.addEventListener("click", () => {
     navList.classList.toggle("hide")
 })
+
+inputDarkMode.addEventListener("click", () => {
+    imgDarkMode.classList.toggle("hide")
+    imgLightMode.classList.toggle("hide")
+    imgDarkModeMain.classList.toggle("hide")
+    imglightModeMain.classList.toggle("hide")
+    tagHtml.classList.toggle("dark-theme")
+})
+
+window.onresize = () => {
+    if (window.matchMedia("(min-width: 940px)").matches) {
+        toggleHeader.remove()
+        if (navList.classList.contains("hide")) {
+            navList.classList.remove("hide")
+        }
+    } else {
+        const divParent = navList.parentNode
+        divParent.insertBefore(toggleHeader, navList)
+        if (!navList.classList.contains("hide")) {
+            navList.classList.add("hide")
+        }
+    }
+}
